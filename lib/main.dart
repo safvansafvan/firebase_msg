1878cebd82f1a325e442ca9cee4810c847292f62
muhammed safvan <sajusajuptl@gmail.com>
2023-12-22 11:48:45 +0530

    firebase initilized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_msg/view/home.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -17,6 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       home: const HomeView(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -5,6 +5,11 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message'),
+        centerTitle: true,
+      ),
+    );
   }
 }
